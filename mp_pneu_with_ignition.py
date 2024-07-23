1af3daad97588176df0bc95c65579601a273b11d
paulsager98 <paulsager98@gmail.com>
2024-07-23 14:58:20 +0200

    firing from pi

--- mp_pneu_with_ignition.py
+++ mp_pneu_with_ignition.py
@@ -3,6 +3,8 @@ import time
 from signal import pause
 import multiprocessing as mp
 from functools import partial
+import atexit
+import signal
 
 
 
@@ -10,6 +12,8 @@ from functools import partial
 
 purge_event = mp.Event()
 purge_done_event = mp.Event()
+start_event = mp.Event()
+termination_event = mp.Event()
 
 
 
@@ -18,27 +22,41 @@ purge_done_event = mp.Event()
 
 
 
-def valve_process_bot(purge_event, purge_done_event):
+
+def valve_process_bot(purge_event, purge_done_event, start_event, termination_event):
 	
 	valv_lower = gpiozero.LED(3)	#Valve 2 (For Lower Chamber)
+	chamber_upper = gpiozero.LED(10)
 	in_upper = gpiozero.Button(8, pull_up = False) #Upper Limit Switch
 	valv_lower.off()
 	
-	def vent_lower(valv_lower):
+	def vent_lower(valv_lower, chamber_upper):
 		print("Upper Rising Trigger")
 		time.sleep(0.02)
+		#Cutter reached top
 		valv_lower.on()
+		time.sleep(0.9) #delay for upper chamber after reaching TDC
+		chamber_upper.on()	#fire chamber
+		time.sleep(10)	#firing duration
+		chamber_upper.off()
 			
 	def close_lower(valv_lower):
 		print("Upper Falling Trigger")
 		time.sleep(0.005)
 		valv_lower.off()
 		
+	def cleanup(valv_lower, chamber_upper):
+		print("Cleaning Up bottom Process")
+		valv_lower.off()
+		chamber_upper.off()
+	
 	print("starting lower process")
-	in_upper.when_pressed = partial(vent_lower, valv_lower)
+
+	
+	in_upper.when_pressed = partial(vent_lower, valv_lower, chamber_upper)
 	in_upper.when_released = partial(close_lower, valv_lower)
 	
-	while True:
+	while not termination_event.is_set():
 		if purge_event.is_set():
 			valv_lower.on()
 			purge_event.clear()
@@ -46,29 +64,48 @@ def valve_process_bot(purge_event, purge_done_event):
 		elif purge_done_event.is_set():
 			valv_lower.on()
 			purge_done_event.clear()
+			
+		elif start_event.is_set():
+			vent_lower(valv_lower, chamber_upper)
+			start_event.clear()
+			
+	print("Exiting lower Process")
+	cleanup(valv_lower, chamber_upper)
+	print("Cleanup Done")
 
 
-def valve_process_top(purge_event, purge_done_event):
+
+def valve_process_top(purge_event, purge_done_event, termination_event):
 	
 	valv_upper = gpiozero.LED(2) #Valve 1 (for Upper Chamber)
+	chamber_lower = gpiozero.LED(9)
 	in_lower = gpiozero.Button(7, pull_up = False) #Lower Limit Switch
 	valv_upper.off()
 	
-	def vent_upper(valv_upper):
+	def vent_upper(valv_upper, chamber_lower):
 		print("Lower Rising Trigger")
 		time.sleep(0.005)
 		valv_upper.on()
+		time.sleep(0.2) #delay for lower chamber after reaching BDC
+		chamber_lower.on()	#fire chamber
+		time.sleep(0.06)	#firing duration
+		chamber_lower.off()
 			
 	def close_upper(valv_upper):
 		print("Lower Falling Trigger")
 		time.sleep(0.4)
 		valv_upper.off()
+		
+	def cleanup(valv_upper, chamber_lower):
+		print("Cleaning Up top Process")
+		valv_upper.off()
+		chamber_lower.off()
 	
 	print("starting upper process")
-	in_lower.when_pressed = partial(vent_upper, valv_upper)
+	in_lower.when_pressed = partial(vent_upper, valv_upper, chamber_lower)
 	in_lower.when_released = partial(close_upper, valv_upper)
 	print("wait for purge")
-	while True:
+	while not termination_event.is_set():
 		if purge_event.is_set():
 			valv_upper.on()
 			purge_event.clear()
@@ -76,6 +113,10 @@ def valve_process_top(purge_event, purge_done_event):
 		elif purge_done_event.is_set():
 			valv_upper.off()
 			purge_done_event.clear()
+			
+	print("Exiting lower Process")
+	cleanup(valv_upper, chamber_lower)
+	print("Cleanup Done")
 
 def purge(purge_event, purge_done_event):
 	purge_event.set()
@@ -118,8 +159,21 @@ def manual_mode():
 			case "E":
 				break
 
-process_bot = mp.Process(target=valve_process_bot, args=(purge_event, purge_done_event))
-process_top = mp.Process(target=valve_process_top, args=(purge_event, purge_done_event))
+def handle_signal(signum, frame, termination_event):
+	print("recived Exit Signal....")
+	termination_event.set()
+	time.sleep(1)
+	
+
+
+	
+	
+signal.signal(signal.SIGTERM, termination_event.set)
+signal.signal(signal.SIGINT, termination_event.set)
+signal.signal(signal.SIGHUP, termination_event.set)
+
+process_bot = mp.Process(target=valve_process_bot, args=(purge_event, purge_done_event, start_event, termination_event))
+process_top = mp.Process(target=valve_process_top, args=(purge_event, purge_done_event, termination_event))
 
 process_bot.start()
 process_top.start()
@@ -132,14 +186,16 @@ match input("Do you want to Purge ? (Y/N), M for Manual Control"):
 
 	case "Y":
 		purge(purge_event, purge_done_event)
+		start_event.set()
 	
 	case "N":
-		pass
+		start_event.set()
 		
 	case "M":
 		manual_mode()
 
-
+input("Press a Key to exit")
+termination_event.set()
 
 process_bot.join()
 process_top.join()
